c36851d13e0de021183aa3fd3666e3bf0ae380ba
Kiryakov Artemiy <triplesentia@gmail.com>
2025-03-10 15:56:14 +0300

    2-nd commit in main

--- Kiryakov.java
+++ Kiryakov.java
@@ -92,7 +92,7 @@ public class Queen {
      * @param g графический контекст, в котором происходит отрисовка ферзя.
     */
     public void paint(Graphics g) {
-        g.setColor(Color.red);
+        g.setColor(Color.green);
         g.fillOval((col()-1)*Desk.CELL_SIZE, (Desk.rowCount() - row())*Desk.CELL_SIZE,
                     Desk.CELL_SIZE, Desk.CELL_SIZE);
     }
